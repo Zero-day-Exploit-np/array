c50eaa037c94685b88fac16c4b000d538b774bb6
Bikram Das <96393941+Zero-day-Exploit-np@users.noreply.github.com>
2026-01-15 23:03:48 +0530

    string

--- string/remove_space.c
+++ string/remove_space.c
@@ -3,13 +3,13 @@
 int main(){
 char str1[]="Hello world";
 int size=strlen(str1);
-int count=0;
 for(int i=0; i<size; i++){
    if(str1[i]==' '){
-       count++;
-     for(int j=i; j<size-count; j++){
-        str1[j]=str1[j+1];
-    }
+       for(int j=i; j<size; j++){
+           str1[j]=str1[j+1];
+        }
+        size--;
+        i--;
    }
 
 }
